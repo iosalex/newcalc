f60395f2719c8ecb3780c7301baab85b0f795529
iosalex <scimoon37@icloud.com>
2026-08-13 22:35:59 +0300

    first

--- Sources/Calculator/routes.swift
+++ Sources/Calculator/routes.swift
@@ -1,11 +1,49 @@
+
 import Vapor
 
+// Что отправляет форма
+struct AddForm: Content {
+    var a: String
+    var b: String
+}
+
+// Что передаётся в шаблон
+struct CalcContext: Content {
+    var a: String
+    var b: String
+    var result: String
+}
+
 func routes(_ app: Application) throws {
-    app.get { req async throws in
-        try await req.view.render("index", ["title": "Hello Vapor!"])
+
+    app.get { req async throws -> View in
+        try await req.view.render(
+            "index",
+            CalcContext(a: "", b: "", result: "")
+        )
     }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    app.post("add") { req async throws -> View in
+        let form = try req.content.decode(AddForm.self)
+
+        let x = Int(form.a) ?? 0
+        let y = Int(form.b) ?? 0
+        let sum = x + y
+
+        return try await req.view.render(
+            "index",
+            CalcContext(
+                a: form.a,
+                b: form.b,
+                result: String(sum)
+            )
+        )
+    }
+
+    app.post("clear") { req async throws -> View in
+        try await req.view.render(
+            "index",
+            CalcContext(a: "", b: "", result: "")
+        )
     }
 }
